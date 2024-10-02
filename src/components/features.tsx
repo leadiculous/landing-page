c39487dd7094b6a59681a04368b35486030c12a3
Sleeyax <yourd3veloper@gmail.com>
2024-10-02 16:33:46 +0200

    fix: hydration errors

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -9,9 +9,7 @@ export const Features = () => {
         </h2>
         <div className="max-w-xl mx-auto">
           <p className="text-center mt-5 text-xl text-white/70">
-            <p>
-              With leadiculous you can save <strong>100+ hours / month</strong> of manual research. Focus on what matters most.
-            </p>
+            With leadiculous you can save <strong>100+ hours / month</strong> of manual research. Focus on what matters most.
           </p>
         </div>
         <div className="flex flex-col items-center justify-center sm:flex-row gap-4">
